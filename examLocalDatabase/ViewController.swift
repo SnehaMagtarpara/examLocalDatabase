89995893355827d2693b5c09444602fc6711c4b3
Sneha magtarpara <snehamagtarpara@gmail.com>
2024-01-02 03:34:50 +0530

    Initial Commit

---
+++ examLocalDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  examLocalDatabase
+//
+//  Created by R93 on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
